3b09ec36ae343b7b738a2fa47c47e6f88a9edde8
SardarBadar <dataentrybadar@gmail.com>
2022-10-23 14:40:45 +0500

    initialcomit

---
+++ android/app/src/main/kotlin/com/example/fdoc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fdoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:fdoc/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'fdoc',
+      home: const LoginScreen()
+    );
+  }
+}
+
